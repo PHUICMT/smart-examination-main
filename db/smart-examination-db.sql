51e988c703bee98a99641d7b45c87e66022e6410
PHUICMT <PHUICMT@github.com>
2022-03-21 08:23:41 +0700

    ✨ Create insert date qurey

--- db/smart-examination-db.sql
+++ db/smart-examination-db.sql
@@ -1,11 +1,11 @@
 USE `smart_examination`;
 
-CREATE TABLE `User` (
+CREATE TABLE `Users` (
   `user_id` varchar(255) PRIMARY KEY,
   `name` varchar(255),
   `surname` varchar(255),
   `start_at` varchar(255),
-  `is_student` bool
+  `is_student` integer
 );
 
 CREATE TABLE `Subject` (
@@ -42,15 +42,53 @@ CREATE TABLE `Examination` (
   `created_at` varchar(255)
 );
 
-ALTER TABLE `Videos` ADD FOREIGN KEY (`student_id`) REFERENCES `User` (`user_id`);
+ALTER TABLE `Videos` ADD FOREIGN KEY (`student_id`) REFERENCES `Users` (`user_id`);
 
 ALTER TABLE `Videos` ADD FOREIGN KEY (`subject_id`) REFERENCES `Subject` (`subject_id`);
 
 ALTER TABLE `Videos` ADD FOREIGN KEY (`exam_pin`) REFERENCES `Examination` (`exam_pin`);
 
-ALTER TABLE `Results` ADD FOREIGN KEY (`student_id`) REFERENCES `User` (`user_id`);
+ALTER TABLE `Results` ADD FOREIGN KEY (`student_id`) REFERENCES `Users` (`user_id`);
 
 ALTER TABLE `Results` ADD FOREIGN KEY (`exam_pin`) REFERENCES `Examination` (`exam_pin`);
 
-ALTER TABLE `Examination` ADD FOREIGN KEY (`teacher_id`) REFERENCES `User` (`user_id`);
+ALTER TABLE `Examination` ADD FOREIGN KEY (`teacher_id`) REFERENCES `Users` (`user_id`);
 
+-- INSERT INTO
+--     `Users` (
+--         `user_id`,
+--         `name`,
+--         `surname`,
+--         `start_at`,
+--         `is_student`
+--     )
+-- VALUES
+--     ("07610497", "Undaman", "Nopnapaporn", "2561", 1),
+--     ("07610400", "Undaman2", "Nopnapaporn2", "2562", 1),
+--     ("11111111", "Teacher", "Test", "2500", 0)
+
+-- INSERT INTO
+--     `Subject` (`subject_id`, `name`)
+-- VALUES
+--     ("Computer", "Computer Programing II")
+
+-- INSERT INTO
+--     `Examination` (
+--         `exam_pin`,
+--         `exam_name`,
+--         `teacher_id`,
+--         `items_count`,
+--         `score`,
+--         `exam`,
+--         `created_at`
+--     )
+-- VALUES
+--     (
+--         "123456",
+--         "Computer Programing II",
+--         "11111111",
+--         10,
+--         10.0,
+--         "[]",
+--         "2562"
+--     )
