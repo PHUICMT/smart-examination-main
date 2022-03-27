e462fd0e300d76940dd70d800b167e0578342004
PHUICMT <PHUICMT@github.com>
2022-03-27 15:04:38 +0700

    ✅ Add exam data format

--- db/smart-examination-db.sql
+++ db/smart-examination-db.sql
@@ -29,7 +29,9 @@ CREATE TABLE `Results` (
   `exam_pin` varchar(255),
   `total_score` integer,
   `emotion` json,
-  `answer` json
+  `answer` json,
+  `start_and_end_time` json,
+  `exam_items_time_stamp` json
 );
 
 CREATE TABLE `Examination` (
@@ -89,6 +91,6 @@ ALTER TABLE `Examination` ADD FOREIGN KEY (`teacher_id`) REFERENCES `Users` (`us
 --         "11111111",
 --         10,
 --         10.0,
---         "[]",
+--         "[{\"article\":1,\"type\":\"Radio\",\"data\":{\"title\":\"1. ขั้นตอนการพัฒนาอัลกอริทึมเพื่อแก้ปัญหา(Develops the algorithm for solution) หมายถึงข้อใด\",\"items\":[\"เอกสารประกอบโปรแกรมจะช่วยอธิบายถึงจุดประสงค์ของโปรแกรม\",\"อัลกอริทึมในที่นี้หมายถึงสูตรทางคณิตศาสตร์ที่ใช้สำหรับในการแก้ปัญญา\",\"การวางแผนการเขียนโปรแกรม ด้วยการออกแบบให้มีเวลาการประมวลผล คือผังงาน(Flowchart)\"]}},{\"article\":2,\"type\":\"TextField\",\"data\":{\"title\":\"2. เขียนโค้ดที่แสดงถึง MVC\"}},{\"article\":3,\"type\":\"CheckBox\",\"data\":{\"title\":\"3. ข้อใดบ้างที่เกี่ยวของกับ React\",\"items\":[\"Fontend\",\"Library\",\"Client\"]}}]",
 --         "2562"
 --     )
